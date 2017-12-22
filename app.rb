30a2f835c54cae6c03cbce749310a6789ba5b41a
Susan Fowler <infopro94@gmail.com>
2017-12-22 18:02:28 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   post '/food_form' do
-    params.to_s
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
   end
 
 
