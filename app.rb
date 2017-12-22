57ddc490748d79eca54ffbb4b6c6ace706559030
Susan Fowler <infopro94@gmail.com>
2017-12-22 18:25:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,9 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  post '/food' do
-    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  get '/food_form' do
+    erb :food_form
   end
 
+  <form method="POST" action="/food">
+    <p>Your Name: <input type="text" name="name"></p>
+    <p>Your Favorite Food: <input type="text" name="favorite_food"></p>
+    <input type="submit">
+  </form>
 
 end
